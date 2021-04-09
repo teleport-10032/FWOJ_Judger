9eaaa8452d8dca489961b882e8bc5fef633cfdfc
Teleport <58069438+teleport-10032@users.noreply.github.com>
2021-04-09 19:00:07 +0800

    Update startJudger.sh

--- startJudger.sh
+++ startJudger.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --rm --cap-add=SYS_PTRACE  -v /home/teleport/CLionProjects/test2/judger:/judger fwoj_judger:v2 bash -c "cd judger && g++ judger.cpp -o judger -std=c++11 && ./judger"
+docker run --rm --cap-add=SYS_PTRACE  -v /home/teleport/CLionProjects/test/judger:/judger yuukiiiqwq/fwoj_judger:v2 bash -c "cd judger && g++ judger.cpp -o judger -std=c++11 && ./judger"
